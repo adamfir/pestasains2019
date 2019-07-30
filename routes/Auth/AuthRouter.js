a804ba99a723d8f40fb8c6b622866b35c3202d59
Adam Firdaus <adam.firdauz22@gmail.com>
2019-07-30 09:13:45 +0700

    rename

--- routes/Auth/AuthRouter.js
+++ routes/Auth/AuthRouter.js
@@ -1,7 +1,7 @@
 let express = require('express'),
     router = express.Router(),
     AuthController = require('./AuthController'),
-    {validateBody, schemas} = require('./Validation.js');
+    {validateBody, schemas} = require('./Validation');
 
 router.post('/registration', validateBody(schemas.schoolRegistration), AuthController.schoolRegistration);
 router.post('/login', validateBody(schemas.schoolLogin), AuthController.schoolLogin);
