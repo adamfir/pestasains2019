e8c809ddd8637f33235633806393447b45959d4e
Adam Firdaus <adam.firdauz22@gmail.com>
2019-08-06 06:43:02 +0700

    test payment gateway

--- routes/Bill/BillController.js
+++ routes/Bill/BillController.js
@@ -11,7 +11,7 @@ class BillController {
         
     }
     static async create(req,res,next){
-        try{ //only for school
+        // try{ //only for school
             let {type, school} = req.value.body;
             let teams = await TeamController.findBySchool(school,{path: 'contest'});
             let teachers = await TeacherController.findBySchool(school);
@@ -46,6 +46,7 @@ class BillController {
                 customer_name : schoolData.name
             }
             let encryptedData = PaymentEncription.encrypt(data,cid,sck);
+            console.log(encryptedData);
             let request = await axios({
                 method: 'post',
                 headers: {'Content-Type':'application/json'},
@@ -54,9 +55,9 @@ class BillController {
             });
             console.log(request);
             res.status(201).json({type, school, teams, teachers, totalPrice});
-        }catch(e){
-            res.status(400).json({message: e.message});
-        }
+        // }catch(e){
+        //     res.status(400).json({message: e.message});
+        // }
     }
     static async callback(req,res,next){
         let {client_id, data} = req.body;
