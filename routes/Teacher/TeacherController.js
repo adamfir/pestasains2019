0fbbb33a35443bb96ec1ca9957c954c4f3261f8f
Adam Firdaus <adam.firdauz22@gmail.com>
2019-09-06 23:38:37 +0700

    Download teacher list

--- routes/Teacher/TeacherController.js
+++ routes/Teacher/TeacherController.js
@@ -1,4 +1,5 @@
 let TeacherModel = require('./TeacherModel');
+let ExcelJS = require('exceljs');
 
 class TeacherController {
     constructor(params) {
@@ -93,6 +94,46 @@ class TeacherController {
             let {school}=req.params,
                 teachers = await TeacherModel.find({accommodationBooking:false,school});
             return res.status(200).json({teachers});
+        } catch (e) {
+            return res.status(400).json({message:e.message});
+        }
+    }
+    static async getExcel(req,res){
+        try {
+            let teachers = await TeacherModel.find({}).populate('school');
+            let workbook = new ExcelJS.Workbook();
+            let worksheet = workbook.addWorksheet("Daftar Guru");
+            worksheet.columns = [
+                {header: 'No', key: 'no', width: 4},
+                {header: 'Nama Guru', key: 'name', width: 20},
+                {header: 'Email', key: 'email', width: 20},
+                {header: 'Telepon', key: 'phone', width: 20},
+                {header: 'NIP', key: 'nip', width: 20},
+                {header: 'Nama Sekolah', key: 'school', width: 20},
+                {header: 'Status Pemabayaran', key: 'isPaid', width: 20},
+            ];
+            let no = 1;
+            for (let i = 0; i < teachers.length; i++,no++) {
+                worksheet.addRow([
+                    no,
+                    teachers[i].name,
+                    teachers[i].email,
+                    teachers[i].phone,
+                    teachers[i].NIP,
+                    teachers[i].school.name,
+                    teachers[i].isPaid,
+                ]);
+                worksheet.commit;
+            }
+            let fileName = "Daftar Guru "+new Date().toISOString().split("T")[0]+'.xlsx';
+            let tempFilePath = __dirname+'/'+fileName;
+            workbook.xlsx.writeFile(tempFilePath).then(()=>{
+                console.log("done");
+                res.setHeader('Content-Disposition', 'attachment; filename=' + fileName);
+                res.sendFile(tempFilePath,(e)=>{
+                    console.log(e);
+                })
+            })
         } catch (e) {
             return res.status(400).json({message:e.message});
         }
--- routes/Teacher/TeacherRouter.js
+++ routes/Teacher/TeacherRouter.js
@@ -13,6 +13,7 @@ router.get('/unpaid/school/:school', JWtController.checkToken, TeacherController
 router.put('/', JWtController.checkToken, JWtController.isSchool, validateBody(schemas.edit), TeacherController.edit);
 router.delete('/:_id', JWtController.checkToken, JWtController.isSchool, TeacherController.delete);
 router.get('/count/:school', JWtController.checkToken, TeacherController.count);
+router.get('/download/excel', JWtController.checkToken, TeacherController.getExcel);
 // router.post('/login', validateBody(schemas.schoolLogin), StudentController.schoolLogin);
 
 module.exports = router;
